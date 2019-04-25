a1131c425527591cdca3b836dd22b4bc6df12e1b
svabra <brayan@brayan.com>
2019-04-25 17:24:02 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,55 +1,116 @@
-$(document).ready(function () {
-    console.log('document ready');
+function Flare(srcEl, trgEl) {
+  this.srcEl = srcEl;
+  this.trgEl = trgEl;
 
-    $("#controls").on('mouseover', 'div', function () {
-        console.log('mouse-over');
-        $("#imgContainer img").removeClass("opaque");
+  this.draw = function() {
 
-        var newImage = $(this).index() + 1;
-        $("#imgContainer img").eq(newImage).addClass("opaque");
-        drawLine($(this));
+    $("header").html("pos (" +
+      this.srcEl.position().left + "," +
+      this.srcEl.position().top + ") (" +
+      this.trgEl.position().left + "," +
+      this.trgEl.position().top + ")"
+    );
 
-    });
 
-    $("#controls").on('mouseout', 'div', function () {
-        console.log('POSITION: ' + $(this).position());
-        
-        $("#imgContainer img").removeClass("opaque");
-        $("#imgContainer img").eq(0).addClass("opaque");
-        clearCanvas();
-    });
 
+  }
+
+  this.update = function() {
+
+    // at the end draw
+    this.draw();
+  }
+}
+
+window.addEventListener('resize', function(event) {
+  // UPDATE THE DRAWINGS AND POSITIONINGS
+  position();
+});
+
+function position() {
+  // resize the canvas area;
+  var c = document.getElementById("myCanvas");
+  // POSITION THE SVAN
+  var x = (window.innerWidth / 2) - ($("#imgContainer>img").eq(0).width() / 2);
+  $("#imgContainer").css({
+    left: x
+  });
+}
+
+// find elements
+var canvas = $("canvas");
+//console.log("canvas width:" + canvas.width);
+var c = document.getElementById("myCanvas");
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+console.log(c);
+
+ctx = c.getContext("2d");
+
+/************/
+var x = 100;
+var y = 100;
+var dx = 0.5;
+var dy = 1;
+var radius = 50
+
+function animate() {
+
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  //ctx.strokeStyle = '#20B2AA';
+  //ctx.stroke(); 
+  ctx.fillStyle = 'rgba(95, 158, 160, 0.8)';
+  ctx.fill();
+
+  if (x + radius > window.innerWidth || x - radius < 0) {
+    dx = -dx;
+  }
+  x += dx;
+  //y += dy;
+}
+
+animate();
+
+
+$("#controls").on('mouseover', 'div', function() {
+  console.log('mouse-over');
+  $("#imgContainer img").removeClass("opaque");
+  var imageIdx = $(this).index() + 1;
+  $("#imgContainer img").eq(imageIdx).addClass("opaque")
+
+  // starte the flare
+  var flare = new Flare($(this), $("#imgContainer"));
+  flare.draw();
 
 });
 
-function clearCanvas() {
-    console.log("clearing canvas");
-    var canvas = document.getElementById('DemoCanvas');
-    if (canvas.getContext) {
-        const context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    }
+$("#controls").on('mouseout', 'div', function() {
+
+  $("#imgContainer img").removeClass("opaque");
+  $("#imgContainer img").eq(0).addClass("opaque");
+  //clearCanvas();
+  $("header").html("HEADER");
+});
+
+position();
+
+var bezier_params = {
+  start: {
+    x: 0,
+    y: 0,
+    angle: 10
+  },
+  end: {
+    x: 200,
+    y: 300,
+    angle: -10,
+    length: 0.25
+  }
 }
 
-function drawLine(element) {
-
-    var canvas = document.getElementById('DemoCanvas');
-    if (canvas.getContext) {
-        console.log("drawing connector line");
-        var context = canvas.getContext('2d');
-        // Reset the current path
-        context.beginPath();
-        // Staring point (10,45)
-        context.moveTo(element.position().left, element.position().top);
-        console.log('printing at: ' + element.position().left + ' ' + element.position().top);
-        // End point (180,47)
-        let imgContainer = $('#imgContainer');
-        context.lineTo(imgContainer.position().left, imgContainer.position().top);
-        console.log('printing to: ' + imgContainer.position().left + ' ' + imgContainer.position().top)
-        context.lineWidth = 2;
-        context.strokeStyle = '#000000';
-        // Make the line visible
-        context.stroke();
-    }
-
-}
+$("#palma").animate({
+  path: new $.path.bezier(bezier_params)
+});
