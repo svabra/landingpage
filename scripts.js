5bd19beec726ae6779a62579d5136a817b2b994a
brayan <brayan@brayan.com>
2019-04-28 16:54:39 +0200

    stable version

--- scripts.js
+++ scripts.js
@@ -11,24 +11,6 @@ function Flare(srcEl, trgEl) {
         this.trgEl.position().top + ")"
       );
   
-      var bezier_params = {
-        start: {
-          x: 185,
-          y: 185,
-          angle: 10
-        },
-        end: {
-          x: 540,
-          y: 110,
-          angle: -10,
-          length: 0.25
-        }
-      }
-  
-      $("#palma").animate({
-        path: new $.path.bezier(bezier_params)
-      });
-  
     }
   
     this.update = function() {
@@ -58,44 +40,19 @@ function Flare(srcEl, trgEl) {
     $("#imgContainer").css({left: x});
   }
   
-  // find elements
-  var canvas = $("canvas");
-  //console.log("canvas width:" + canvas.width);
+
+
+///////////// INVOCATIONS CODE //////////////////
+$(document).ready(function(){
+
+  //var canvas = $("canvas");
   var c = document.getElementById("myCanvas");
   c.width = window.innerWidth;
   c.height = window.innerHeight;
   console.log(c);
   
   ctx = c.getContext("2d");
-  
-  /************/
-  var x = 100;
-  var y = 100;
-  var dx = 0.5;
-  var dy = 1;
-  var radius = 50
-  
-  function animate() {
-  
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    //ctx.strokeStyle = '#20B2AA';
-    //ctx.stroke(); 
-    ctx.fillStyle = 'rgba(95, 158, 160, 0.8)';
-    ctx.fill();
-  
-    if (x + radius > window.innerWidth || x - radius < 0) {
-      dx = -dx;
-    }
-    x += dx;
-    //y += dy;
-  }
-  
-  animate();
-  
-  
+
   $("#controls").on('mouseover', 'div', function() {
     console.log('mouse-over');
     $("#imgContainer img").removeClass("opaque");
@@ -120,7 +77,7 @@ function Flare(srcEl, trgEl) {
   
   
   // CreateJS TweenJS
-  var stage = new createjs.Stage("myCanvas"); 
+  var stage = new createjs.Stage("newCanvas"); 
   var circle = new createjs.Shape();
   circle.graphics.beginFill("red").drawCircle(100, 100, 50);
   circle.x = 100;
@@ -128,4 +85,10 @@ function Flare(srcEl, trgEl) {
   stage.addChild(circle);
   
   stage.update(); 
+
   
+  
+
+  
+  
+});
